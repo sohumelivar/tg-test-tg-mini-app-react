402661a2ccf0d7ac3135d80d227000248fc366e9
Givi Koyava <givikoyava@iMac-Givi.local>
2024-09-04 20:17:26 +0300

    added test func

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import { useEffect } from 'react';
-const tg = window.Telegram.WebApp;
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const {tg, onToggleButton} = useTelegram
+
 
   useEffect(() => {
     tg.ready();
   }, [])
 
-  const onClose = () => {
-    tg.close();
-  }
-
   return (
     <div className="App">
-        work
-          <button onClick={onClose} >Close App</button>
+        <button onClick={onToggleButton} >Toggle</button>
     </div>
   );
 }
