1961f8176f8167d8a49d29424842779510e1c6f1
Givi Koyava <givikoyava@iMac-Givi.local>
2024-09-04 20:53:56 +0300

    test3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import Button from '../Button/Button'
-import { useTelegram } from '../../hooks/useTelegram'
+import React from 'react';
+import Button from '../Button/Button';
+import { useTelegram } from '../../hooks/useTelegram';
+import './Header.css'
 
 export default function Header() {
     const {user, onClose} = useTelegram();
