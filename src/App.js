def03129a9a816e187eb2059eded0b27bbfff06c
Givi Koyava <givikoyava@iMac-Givi.local>
2024-09-08 15:13:11 +0300

    test7

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   return (
     <div className="App">
         <Header />
+        <input placeholder='eбаный фильтр! нажимай !'/>
         <Routes>
           <Route index element={<ProductList />} />
           <Route path={'form'} element={<Form />} />
